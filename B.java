6217f74adb8cc6a24d32f94257ba3d39d9ef6c02
AniketDuryodhan <aniketduryodhan12@gmail.com>
2022-08-25 22:29:05 +0530

    Myfirstcommit

---
+++ B.java
@@ -0,0 +1,50 @@
+ublic class B
+{
+
+
+static int u =10 ;
+
+
+
+
+	public static void main(String [] args)
+	{
+		
+		byte a =10;
+		int b = 200000;
+		float f = 12.23f; //float f = 15.45F;
+		double d =45.6666666666666666d; //double d =48.666666D; double d = 75.36
+		long l = 4588885555555l; //long l = 445585665552L;
+		
+		char c = 'a';
+		char d1 = 'b';
+		
+		String s = "10";
+		String p="34.0999";
+		String m = "gunjan kathale";
+		String k = "k";
+		
+		boolean o = true;
+		boolean h = false;
+		
+		
+	System.out.println("byte :-" + a);
+	System.out.println("int :- "+b);
+	System.out.println("float :" + f);
+	System.out.println("double :-" +d);
+	System.out.println("long :-" + l);
+	System.out.print("char :-"+c);
+	System.out.print("char :-"+d1);
+	System.out.print("String :-"+s);
+	System.out.println("String :-"+p);
+	System.out.println("String :-"+m);
+	System.out.println("String :-"+k);
+	System.out.println("boolean :-"+o);
+	System.out.println("boolean :-"+h);
+	System.out.println("gloabal variable :-"+u);
+	}
+	
+
+	
+	
+}
