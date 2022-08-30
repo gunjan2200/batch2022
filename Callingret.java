e72f02827fad9d6fbd48a6917d182cc629c136d6
AniketDuryodhan <aniketduryodhan12@gmail.com>
2022-08-30 19:39:27 +0530

    adding updated file

--- Callingret.java
+++ Callingret.java
@@ -4,15 +4,17 @@ public int method1( int a  , int b)
 {
 	System.out.println("This is method1");
 	int c = a + b ;  
-	return c;
+	return c;                              // Return type method 
 }
 	
 	public static void main (String[] args)
 	{
 		System.out.println("this is  main method");
-		Callingret c = new Callingret();
-		int v = c.method1(25 , 56);
+		Callingret d = new Callingret();
+		int v = d.method1(25 , 56);                       //Storing the value of c  into  v by declaring it as integer 
 		System.out.println("addition of number is" +v);
 	}
 	
 }
+
+/** Calling return type method  
